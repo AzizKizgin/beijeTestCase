fcecb52e6620def1bb6f72940e7d32f573217bde
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-09 15:56:10 +0300

    feat: border added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -25,6 +25,8 @@ const Header = () => {
       justifyContent={'space-between'}
       paddingY={2}
       paddingX={6}
+      borderBottomWidth={1}
+      borderBottomColor={'gray.200'}
       backgroundColor={'bgColor'}>
       <Logo onPress={onLogoPress} />
       <HStack space={4} alignItems={'center'} justifyContent={'center'}>
