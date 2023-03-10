900673b0e537da8f8153ffc17f52af854c99666c
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-10 17:04:29 +0300

    feat: new shop cards added

--- src/components/PocketModal/index.tsx
+++ src/components/PocketModal/index.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {Box, Text, VStack} from 'native-base';
+import {Box, ScrollView, Text, VStack} from 'native-base';
 import Modal from 'react-native-modal';
 import ModalHeader from './ModalHeader';
 import ModalInfo from './ModalInfo';
@@ -7,6 +7,9 @@ import ModalImage from './ModalImage';
 import {useShopping} from '../../context/ShoppingContext';
 import PadShopCard from './PadShopCard';
 import {strings} from '../../utils/strings';
+import DailyPadShopCard from './DailyPadShopCard';
+import BufferShopCard from './BufferShopCard';
+import Button from '../Button';
 
 interface PocketModalProps {
   modalVisible: boolean;
@@ -16,6 +19,16 @@ interface PocketModalProps {
 const PocketModal: FC<PocketModalProps> = (props) => {
   const {modalVisible, setModalVisible} = props;
   const {products} = useShopping();
+
+  const standardPad = products.find((i) => i.title === strings.standardPad);
+  const superPad = products.find((i) => i.title === strings.superPad);
+  const superPlusPad = products.find((i) => i.title === strings.superPlusPad);
+  const dailyPad = products.find((i) => i.title === strings.dailyPad);
+  const superDailyPad = products.find((i) => i.title === strings.superDailyPad);
+  const miniBuffer = products.find((i) => i.title === strings.miniBuffer);
+  const standardBuffer = products.find(
+    (i) => i.title === strings.standardBuffer,
+  );
   return (
     <Modal
       isVisible={modalVisible}
@@ -25,30 +38,39 @@ const PocketModal: FC<PocketModalProps> = (props) => {
       onBackdropPress={() => setModalVisible(false)}
       animationInTiming={600}
       style={{
-        justifyContent: 'center',
+        marginVertical: 10,
       }}>
-      <Box
-        backgroundColor={'bgColor'}
-        paddingY={5}
-        paddingX={4}
-        borderRadius={'md'}>
-        <ModalHeader
-          onClose={() => {
-            setModalVisible(false);
-          }}
-        />
-        <VStack space={6}>
-          <ModalInfo />
-          <ModalImage />
-          <PadShopCard
-            standartPad={products.find((i) => i.title === strings.standardPad)}
-            superPad={products.find((i) => i.title === strings.superPad)}
-            superPlusPad={products.find(
-              (i) => i.title === strings.superPlusPad,
-            )}
+      <ScrollView>
+        <Box
+          backgroundColor={'bgColor'}
+          paddingY={5}
+          paddingX={4}
+          borderRadius={'md'}>
+          <ModalHeader
+            onClose={() => {
+              setModalVisible(false);
+            }}
           />
-        </VStack>
-      </Box>
+          <VStack space={6}>
+            <ModalInfo />
+            <ModalImage />
+            <PadShopCard
+              standartPad={standardPad}
+              superPad={superPad}
+              superPlusPad={superPlusPad}
+            />
+            <DailyPadShopCard
+              dailyPad={dailyPad}
+              superDailyPad={superDailyPad}
+            />
+            <BufferShopCard
+              miniBuffer={miniBuffer}
+              standardBuffer={standardBuffer}
+            />
+            <Button title={strings.addToCart} onPress={() => {}} />
+          </VStack>
+        </Box>
+      </ScrollView>
     </Modal>
   );
 };
