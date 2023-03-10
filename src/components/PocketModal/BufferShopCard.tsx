8972b867cbce5ece20c552da4a0344b4ac985a6c
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-10 17:04:11 +0300

    feat: new shop cards addded

---
+++ src/components/PocketModal/BufferShopCard.tsx
@@ -0,0 +1,55 @@
+import React, {FC} from 'react';
+import {Box, HStack, Pressable, Text} from 'native-base';
+import {strings} from '../../utils/strings';
+import {useShopping} from '../../context/ShoppingContext';
+
+interface BufferShopCardProps {
+  miniBuffer?: Product;
+  standardBuffer?: Product;
+}
+
+const BufferShopCard: FC<BufferShopCardProps> = (props) => {
+  const {
+    miniBuffer = {
+      title: strings.dailyPad,
+      count: 0,
+    },
+    standardBuffer = {
+      title: strings.superDailyPad,
+      count: 0,
+    },
+  } = props;
+
+  const {removeItem} = useShopping();
+
+  if (miniBuffer.count === 0 && standardBuffer.count === 0) {
+    return null;
+  }
+  return (
+    <Box backgroundColor={'white'} padding={4}>
+      <Text fontSize={'lg'} color={'darkGray'} fontWeight={'semibold'}>
+        {strings.padPacks}
+      </Text>
+      <Text fontSize={'sm'} color={'gray.400'}>
+        {miniBuffer.count > 0
+          ? `${miniBuffer.count} ${strings.miniBuffer} `
+          : ''}
+        ,
+        {standardBuffer.count > 0
+          ? `${standardBuffer.count} ${strings.superDailyPad} `
+          : ''}
+      </Text>
+      <Pressable
+        marginTop={2}
+        onPress={() => {
+          removeItem(miniBuffer, standardBuffer);
+        }}>
+        <Text fontSize={'sm'} color={'darkGray'} fontWeight={'semibold'}>
+          {strings.removefromPack}
+        </Text>
+      </Pressable>
+    </Box>
+  );
+};
+
+export default BufferShopCard;
---
+++ src/components/PocketModal/DailyPadShopCard.tsx
@@ -0,0 +1,52 @@
+import React, {FC} from 'react';
+import {Box, HStack, Pressable, Text} from 'native-base';
+import {strings} from '../../utils/strings';
+import {useShopping} from '../../context/ShoppingContext';
+
+interface DailyPadShopCardProps {
+  dailyPad?: Product;
+  superDailyPad?: Product;
+}
+
+const DailyPadShopCard: FC<DailyPadShopCardProps> = (props) => {
+  const {
+    dailyPad = {
+      title: strings.dailyPad,
+      count: 0,
+    },
+    superDailyPad = {
+      title: strings.superDailyPad,
+      count: 0,
+    },
+  } = props;
+
+  const {removeItem} = useShopping();
+
+  if (dailyPad.count === 0 && superDailyPad.count === 0) {
+    return null;
+  }
+  return (
+    <Box backgroundColor={'white'} padding={4}>
+      <Text fontSize={'lg'} color={'darkGray'} fontWeight={'semibold'}>
+        {strings.padPacks}
+      </Text>
+      <Text fontSize={'sm'} color={'gray.400'}>
+        {dailyPad.count > 0 ? `${dailyPad.count} ${strings.dailyPad} ` : ''},
+        {superDailyPad.count > 0
+          ? `${superDailyPad.count} ${strings.superDailyPad} `
+          : ''}
+      </Text>
+      <Pressable
+        marginTop={2}
+        onPress={() => {
+          removeItem(dailyPad, superDailyPad);
+        }}>
+        <Text fontSize={'sm'} color={'darkGray'} fontWeight={'semibold'}>
+          {strings.removefromPack}
+        </Text>
+      </Pressable>
+    </Box>
+  );
+};
+
+export default DailyPadShopCard;
