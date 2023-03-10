6c755b41159d10625e6e6171a37caf74d7c24939
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-10 17:03:57 +0300

    feat: custom button added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {Pressable, Text} from 'native-base';
 import {useShopping} from '../context/ShoppingContext';
 import {strings} from '../utils/strings';
 
-const MenuButton = () => {
-  const {openModal} = useShopping();
+interface ButtonProps {
+  onPress: () => void;
+  title: string;
+}
+const Button: FC<ButtonProps> = (props) => {
+  const {onPress, title} = props;
   return (
     <Pressable
-      onPress={openModal}
+      onPress={onPress}
       backgroundColor="darkGray"
       paddingY={4}
       justifyContent="center"
@@ -15,10 +19,10 @@ const MenuButton = () => {
       marginX={4}
       borderRadius={'full'}>
       <Text color="white" fontSize={'md'}>
-        {strings.seePack}
+        {title}
       </Text>
     </Pressable>
   );
 };
 
-export default MenuButton;
+export default Button;
