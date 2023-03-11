7aa9127dfb48f6261d84d4869b2cc72b72026e5e
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-11 17:49:27 +0300

    feat: toast message added

--- App.tsx
+++ App.tsx
@@ -5,15 +5,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import Router from './src/navigation/Router';
 import {ShoppingProvider} from './src/context/ShoppingContext';
 import {BottomMenuProvider} from './src/context/BottomMenuContext';
+import {ToastMessageProvider} from '@aziz_kizgin/react-native-toast-message';
 
 const App = () => {
   return (
     <NativeBaseProvider theme={appTheme}>
       <ShoppingProvider>
         <BottomMenuProvider>
-          <NavigationContainer>
-            <Router />
-          </NavigationContainer>
+          <ToastMessageProvider>
+            <NavigationContainer>
+              <Router />
+            </NavigationContainer>
+          </ToastMessageProvider>
         </BottomMenuProvider>
       </ShoppingProvider>
     </NativeBaseProvider>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,9 +4,31 @@ import MenuTabs from '../navigation/MenuTabs';
 import {strings} from '../utils/strings';
 import Button from '../components/Button';
 import {useShopping} from '../context/ShoppingContext';
-
+import {useToast} from '@aziz_kizgin/react-native-toast-message';
+import appTheme from '../theme/appTheme';
 const HomeScreen = () => {
-  const {openModal} = useShopping();
+  const {openModal, products} = useShopping();
+  const {showToast} = useToast();
+  const totalPrice = products.reduce((total, item) => {
+    return total + 3 * item.count;
+  }, 0);
+
+  const onPress = () => {
+    if (totalPrice === 0) {
+      showToast({
+        color: 'warning',
+        message: strings.noProduct,
+        position: 'top',
+        duration: 2000,
+        textStyle: {
+          color: appTheme.colors.darkGray,
+          fontWeight: '500',
+        },
+      });
+    } else {
+      openModal();
+    }
+  };
   return (
     <Box flex={1} backgroundColor="bgColor" paddingX={1}>
       <VStack paddingX={4} space={2} marginY={4}>
@@ -23,7 +45,7 @@ const HomeScreen = () => {
       </VStack>
       <Box>
         <MenuTabs />
-        <Button title={strings.seePack} onPress={openModal} />
+        <Button title={strings.seePack} onPress={onPress} />
       </Box>
     </Box>
   );
